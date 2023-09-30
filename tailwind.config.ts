d676d09621453b506d641020594378178638c508
Luis Cislaghi <luis.cislaghi@gmail.com>
2023-09-30 01:07:07 -0300

    fix dashed border

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -9,8 +9,7 @@ const config: Config = {
   theme: {
     extend: {
       backgroundImage: {
-        "dashed-border":
-          'url("data:image/svg+xml,%3csvg width="100%25" height="100%25" xmlns="http://www.w3.org/2000/svg"%3e%3crect width="100%25" height="100%25" fill="none" rx="24" ry="24" stroke="%23333" stroke-width="2" stroke-dasharray="6%2c 12" stroke-dashoffset="6" stroke-linecap="square"/%3e%3c/svg%3e")',
+        "dashed-border": 'url("/dashed-border.svg")',
         "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
         "gradient-conic":
           "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
