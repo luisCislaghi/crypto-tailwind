16524a14f94ddebb23e7aa470ab8cbd424649828
Luis Cislaghi <luis.cislaghi@gmail.com>
2023-11-27 21:24:04 -0300

    add design credits

--- src/app/dashboard/layout.tsx
+++ src/app/dashboard/layout.tsx
@@ -14,6 +14,16 @@ export default function DashboardLayout({
         <div className="grow">
           {header}
           <div className="flex min-h-screen items-start">{children}</div>
+          <div className=" w-full p-6 text-center text-stone-500">
+            Design by{" "}
+            <a
+              className="font-semibold hover:underline hover:decoration-2 hover:underline-offset-4 "
+              target="_blank"
+              href={"https://dribbble.com/shots/18616332-Palmer-Crypto-Web-App"}
+            >
+              @Arauda
+            </a>
+          </div>
         </div>
       </section>
     </>
